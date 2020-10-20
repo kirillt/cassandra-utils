94c405dcc8ad62d906f5e182c4ab7b6affb71fe3
Kirill Taran <kirill@taran.space>
2020-10-20 23:13:50 +0200

    Log persistence, displaying elapsed time

--- dump-to-csv.sh
+++ dump-to-csv.sh
@@ -6,37 +6,54 @@ dir=`date +%s`
 pushd() { command pushd "$@" > /dev/null; }
 popd() { command popd "$@" > /dev/null; }
 
-indent() { sed -u 's/^/    /' "$@"; }
-
 echo "Creating snapshot folder $dir"
 mkdir $dir
 pushd $_
+echo
+
+indent() { sed -u 's/^/    /' "$@"; }
+
+start=$SECONDS
 
 dump-table() {
     keyspace=$1
     table=$2
 
-    echo "Dumping table ''$table''"
+    echo "Dumping table ''$keyspace.$table''"
 
     # sed is necesary here, because variables inside of '' are not expanded
     cmd=$(echo "copy %table% to '%table%.csv'" | sed s/%table%/$table/g)
 
     cqlsh -k $keyspace -e "$cmd" | indent
     echo
-    exit
+
+    echo "Total seconds elapsed: $(($SECONDS - $start))"
+    echo
 }
 
-keyspaces=$(cqlsh -e 'describe keyspaces;')
-for keyspace in $keyspaces
-do
-    echo "Dumping keyspace ''$keyspace''"
-    mkdir $keyspace
-    pushd $_
-    tables=$(cqlsh -k $keyspace -e 'describe tables;')
-    for table in $tables
+dump-all() {
+    keyspaces=$(cqlsh -e 'describe keyspaces;')
+    for keyspace in $keyspaces
     do
-        dump-table $keyspace $table | indent
+        echo "Dumping keyspace ''$keyspace''"
+        mkdir $keyspace
+        pushd $_
+        tables=$(cqlsh -k $keyspace -e 'describe tables;')
+        for table in $tables
+        do
+            dump-table $keyspace $table | indent
+        done
+        popd
     done
-    popd
-done
+}
+
+log="output.log"
+
+dump-all 2>&1 | tee $log
 popd
+
+echo "Log with all errors:"
+echo "$dir/$log" | indent
+
+echo "Snapshot folder:"
+du -sh $dir | indent
